cd8c95412a8c3e974c5dd3bc9d5587b74a4253ea
Alwin Philip <151048553+Alwin-06@users.noreply.github.com>
2023-12-24 23:35:21 +0530

    Create 3_smallestnumber.c

---
+++ cycle1/3_smallestnumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a[20],n,i,*small;
+    small=a;
+    printf("How many numbers ? : ");
+    scanf("%d",&n);
+    printf("Enter the elements : ");
+    for (i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for (i=0;i<n;i++)
+    {
+        if (*(a+i) < *small)
+            *small=*(a+i);
+    }
+    printf("Smallest Number : %d\n",*small);
+}
